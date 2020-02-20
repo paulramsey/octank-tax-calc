220ac7eb89df8463fb2664a94c37d27b0a5f4fe2
Paul Ramsey <22893884+paulramsey@users.noreply.github.com>
2020-02-20 10:08:27 -0600

    Add X-Ray tracing

--- src/main/java/com/octank/taxcalc/TaxCalcApplication.java
+++ src/main/java/com/octank/taxcalc/TaxCalcApplication.java
@@ -1,13 +1,19 @@
 package com.octank.taxcalc;
 
+import java.util.Arrays;
+
+import javax.servlet.Filter;
+
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import java.util.Arrays;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class TaxCalcApplication extends SpringBootServletInitializer {
@@ -35,4 +41,12 @@ public class TaxCalcApplication extends SpringBootServletInitializer {
 		};
 	}
 
+	@Configuration
+	public class WebConfig {
+
+		@Bean
+		public Filter TracingFilter() {
+			return new AWSXRayServletFilter("/tax-calc");
+		}
+	}
 }
--- src/main/java/com/octank/taxcalc/TaxCalcController.java
+++ src/main/java/com/octank/taxcalc/TaxCalcController.java
@@ -1,12 +1,13 @@
 package com.octank.taxcalc;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.context.annotation.Bean;
+import com.amazonaws.xray.AWSXRay;
 
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
@@ -16,20 +17,26 @@ public class TaxCalcController {
 	public String index() {
 
 		// Generate some load by calculating pi
+		AWSXRay.beginSubsegment("Running calulations");
 		Pi.computePi(20000);
+		AWSXRay.endSubsegment();
 
 		// Generate random tax rate
+		AWSXRay.beginSubsegment("Deriving tax rate");
 		double taxRate = Math.random() * 9 + 1;
 		if (taxRate < 5) {
 			taxRate = taxRate + 5;
 		}
 		taxRate = taxRate/100;
 		taxRate = Math.round(taxRate * 10000.0) / 10000.0;
+		AWSXRay.endSubsegment();
 
 		// Generate random total tax
+		AWSXRay.beginSubsegment("Calculate total tax");
 		double totalTax = Math.random() * 2000 + 1;
 		totalTax = totalTax * taxRate;
 		totalTax = Math.round(totalTax * 100.0) / 100.0;
+		AWSXRay.endSubsegment();
 
 		String rtrn = "{\"taxRate\": \"" + String.valueOf(taxRate) + "\", \"totalTax\": \"" + String.valueOf(totalTax) + "\"}";
 		//System.out.println(rtrn);
